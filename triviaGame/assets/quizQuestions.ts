63233fd2236c59cbc5be9232bb5d3eaf1792f2cd
marc <mfmunic@gmail.com>
2018-12-11 15:57:28 -0800

    Auto-generated commit

---
+++ triviaGame/assets/quizQuestions.ts
@@ -0,0 +1,76 @@
+export const trivia = [
+  {
+    question: "Which POS city does not exist?",
+    wrong: ["Shit", "Poo", "Poop"],
+    correct: "Dung",
+    explain: [
+      "There are at least three villages in Iran with this name: one in Kermanshah Province, one in Mazandaran Province, and one in Zanjan Province.",
+      "A town in Himachal Pradesh, India.",
+      "A town in Yucatan, Mexico, Next to Ekpedz, Mexico."
+    ]
+  },
+  {
+    question: "Which murder town is fake?",
+    wrong: ["Slaughter", "Kill", "Cut and Shoot"],
+    correct: "Knife",
+    explain: [
+      "Renamed Auburn, Washington",
+      "A village in County Kildare, Ireland.",
+      "A town in Texas northeast of Conroe, Texas, On Texas Route 105."
+    ]
+  },
+  {
+    question: "Which place is so dumb it was never made?",
+    wrong: ["Stupid Lake", "Idiotville", "Moron"],
+    correct: "Dumb",
+    explain: [
+      "A lake in Manitoba, Canada",
+      "A ghost town in Oregon.",
+      "An Argentine city."
+    ]
+  },
+  {
+    question: "What area do you need to smoke to actually find?",
+    wrong: ["Bong County", "Ganja", "Weed"],
+    correct: "Marijuana Forest Park",
+    explain: [
+      "A county in Liberia, named for its Mount Bong.",
+      "A city in Azerbaijan.",
+      "Town in Siskiyou County, California"
+    ]
+  },
+  {
+    question: "Which Boring is too boring to be real?",
+    wrong: ["Boring, Maryland", "Boring, Oregon", "Boring, Tennessee"],
+    correct: "Boring, Nebraska",
+    explain: [
+      "Named for a former postmaster David Boring, whose profession suited the family name.",
+      "Twinned with Dull, Scotland.",
+      "Well-named: as of May 2017, the length of its Wikipedia entry is 1,338 characters."
+    ]
+  },
+  {
+    question: "Which Mount could use some levity and become a place?",
+    wrong: ["Mount Disappointment", "Mount Despair", "Mount Terror"],
+    correct: "Mount Doom",
+    explain: [
+      "A rather uninspiring hill on the edge of Melbourne.",
+      "Amazingly, there are three different mountains with this name, two in the USA and one in Australia.",
+      "Names of two mountains, one in Antarctica, the other in Washington. Great places for a family picnic."
+    ]
+  },
+  {
+    question: "Which name is just me hitting keys on the keyboard?",
+    wrong: [
+      "Lake Chargoggagoggmanchauggagoggchaubunagungamaugg",
+      "Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch",
+      "El Pueblo de Nuestra Senora la Reina de los Angeles de Porciuncula"
+    ],
+    correct: "Gcwjogheshdfkjhgakhlkda County",
+    explain: [
+      "The longest place name in the United States and 6th longest in the world.",
+      "The longest officially recognised place name in the United Kingdom. It is found in Wales near the Isle of Anglesey and is situated near the A5",
+      "The original name of Los Angeles. The name means The Town of Our Lady the Queen of the Angels of Porziuncola."
+    ]
+  }
+];
